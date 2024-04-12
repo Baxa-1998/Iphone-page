62593d69feb3c2933399535ecc7ac54235346aca
Baxa-1998 <baxtiyornarziev6@gmail.com>
2024-04-12 08:01:56 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import './App.css'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Highlight from './components/Highlight'
+import Model from './components/Model'
+
+function App() {
+ 
+
+  return (
+    <main className='bg-black'> 
+    <Navbar/> 
+    <Hero/>
+    <Highlight/>
+    <Model/>
+
+    </main>
+    
+  )
+}
+
+export default App
